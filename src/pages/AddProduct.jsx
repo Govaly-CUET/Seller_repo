c187b66ab997b5358660180953e8c33699f52e2d
blackcodd <u2104046@student.cuet.ac.bd>
2026-09-15 11:10:01 +0600

    update api

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -34,9 +34,7 @@ const AddProduct = () => {
             uploadData.append('file', file);
             uploadData.append('folder', 'govaly/products');
 
-            const res = await axiosInstance.post('/api/v1/seller/upload', uploadData, {
-                headers: { 'Content-Type': 'multipart/form-data' },
-            });
+            const res = await axiosInstance.post('/api/v1/upload', uploadData);
 
             const uploadedUrl = res.data.data.url;
             setFormData((prev) => ({ ...prev, image: uploadedUrl }));
